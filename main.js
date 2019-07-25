4f64b5e0c89a78f067ce5e9903380ccb6227cc8e
Aleksey Matveev <matveevalex1512@gmail.com>
2019-07-26 01:35:51 +0300

    add BL

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+function check(event){
+    console.log(event.type);           
+
 window.onload=function(){    
     var btn = document.getElementsByClassName("btn");
-    for (var i = 0; i < btn.length; i++){
-        btn[i].addEventListener("click", check, false);
+    var arr = new Array();
+    for (var i = 0; i < btn.length; i++){        
+        btn[i].addEventListener("click", check);
+        arr[i] = parseInt(Math.random()*(99-1)+1);
+        btn[i].innerHTML = arr[i];
     }
-
-    function check(e){
-        console.log("click");
-    }
+    
 }
