8fc89f8bfa6fec8585e22481b06c9fca85cb8dd5
Aleksey Matveev <matveevalex1512@gmail.com>
2019-07-25 21:04:06 +0300

    fix

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-window.onload=function(){
-    console.log("start");
-var btn = document.getElementsByClassName("btn");
-console.log(btn);
-for (var i = 0; i < btn.length; i++){
-    console.log(btn[i].id);
-btn[i].addEventListener("click", check, false);
-}
+window.onload=function(){    
+    var btn = document.getElementsByClassName("btn");
+    for (var i = 0; i < btn.length; i++){
+        btn[i].addEventListener("click", check, false);
+    }
 
-function check(e){
-    console.log("click");
-}
+    function check(e){
+        console.log("click");
+    }
 }
