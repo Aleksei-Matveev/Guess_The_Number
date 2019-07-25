4a5b70a5005b483bd8c61a4e59cfde72be81bb05
Aleksey Matveev <matveevalex1512@gmail.com>
2019-07-26 01:37:18 +0300

    add BL

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-function check(event){
-    console.log(event.type);           
+var arr = new Array();
+var count=0;
 
 window.onload=function(){    
     var btn = document.getElementsByClassName("btn");
-    var arr = new Array();
+
+    initgame();
+   
     for (var i = 0; i < btn.length; i++){        
         btn[i].addEventListener("click", check);
-        arr[i] = parseInt(Math.random()*(99-1)+1);
+       
         btn[i].innerHTML = arr[i];
+        btn[i].value = arr[i];
+    }
+    arr.sort(function(a , b){return a-b;});
+}
+
+function initgame(){
+    for (var i = 0; i < 16; i++){
+		var numberRand = parseInt(Math.random()*(99-1)+1);
+		for (var y = 0; y <= i; y++) {
+
+			if (numberRand == arr[y]) {
+				numberRand = parseInt(Math.random()*(99-1)+1);
+				y = 0;
+			}
+		}
+        arr[i] = numberRand;        
     }
     
+    count=0;
+}
+
+function check(){
+    console.log(arr);
+    console.log(arr[count]);
+    console.log(parseInt(this.value))
+
+    if (arr[count]==parseInt(this.value)){
+        console.log("true");
+        count++;
+        this.disabled=true;
+    }
+    else
+     console.log("false");
+
 }
